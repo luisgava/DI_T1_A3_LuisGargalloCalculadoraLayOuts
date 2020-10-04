b6d9568402961aa371dfc76e3da396faab102e3d
luisgava <63642035+luisgava@users.noreply.github.com>
2020-10-04 15:33:53 +0200

    grid terminado

--- src/calculadoras/CalculadoraGridApp.java
+++ src/calculadoras/CalculadoraGridApp.java
@@ -1,7 +1,7 @@
 package calculadoras;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -16,20 +16,35 @@ import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Toolkit;
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
 
 public class CalculadoraGridApp extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
-	private String num1=null;	
-	private String num2=null;
+	private String num1 = "0";
+	private String num2 = "0";
 	private String operacion;
-	
+
+	private JButton btn7;
+	private JButton btn8;
+	private JButton btn9;
+	private JButton btn4;
+	private JButton btn5;
+	private JButton btn6;
+	private JButton btn1;
+	private JButton btn2;
+	private JButton btn3;
+	private JButton btn0;
+
+	private JButton btnComa;
+	private JButton btnClear;
+
 	private JButton btnSuma;
+	private JButton btnResta;
+	private JButton btnMultiplicacion;
+	private JButton btnDivision;
+	private JButton btnResultado;
+
 	private JLabel lblOperacion;
 
 	/**
@@ -41,6 +56,7 @@ public class CalculadoraGridApp extends JFrame {
 				try {
 					CalculadoraGridApp frame = new CalculadoraGridApp();
 					frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -49,14 +65,14 @@ public class CalculadoraGridApp extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Clase para crear el frame
 	 */
 	public CalculadoraGridApp() {
-		
 
+		// Frame
 		setIconImage(Toolkit.getDefaultToolkit().getImage("src\\img\\calculadora.png"));
-		setResizable(false);
-		setTitle("Calculadora Grid");
+		setResizable(true);
+		setTitle("Calculadora Grid");		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 241, 330);
 		contentPane = new JPanel();
@@ -65,7 +81,16 @@ public class CalculadoraGridApp extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(5, 4, 5, 5));
 
-		JButton btnResultado = new JButton("=");
+		// Listeners
+		ListenerBotonNumerico listenerBotonNumerico = new ListenerBotonNumerico();
+		ListenerBotonOperacion listenerBotonOperacion = new ListenerBotonOperacion();
+		ListenerBotonIgual listenerBotonIgual = new ListenerBotonIgual();
+		ListenerBotonComa listenerBotonComa = new ListenerBotonComa();
+		ListenerBotonClear listenerBotonClear = new ListenerBotonClear();
+
+		// Botón =
+		btnResultado = new JButton("=");
+		btnResultado.addActionListener(listenerBotonIgual);		
 		btnResultado.setBackground(new Color(245, 222, 179));
 		contentPane.add(btnResultado);
 
@@ -73,123 +98,199 @@ public class CalculadoraGridApp extends JFrame {
 		JLabel lblBlanca1 = new JLabel("");
 		contentPane.add(lblBlanca1);
 
+		// TextField para los números y el resultado
 		textField = new JTextField();
 		textField.setBackground(Color.WHITE);
 		textField.setEditable(false);
 		contentPane.add(textField);
 		textField.setColumns(10);
 
-		// Label para dejar espacio blanco en el grid
+		// Label operación
 		lblOperacion = new JLabel("");
 		contentPane.add(lblOperacion);
 
-		// Listeners
-		ListenerBotonNumerico listenerBotonNumerico = new ListenerBotonNumerico();
-		ListenerBotonOperacion listenerBotonOperacion = new ListenerBotonOperacion();
-
-		JButton btn7 = new JButton("7");
+		// Botón 7
+		btn7 = new JButton("7");
 		btn7.addActionListener(listenerBotonNumerico);
 		btn7.setBackground(new Color(250, 128, 114));
 		contentPane.add(btn7);
 
-		JButton btn8 = new JButton("8");
+		// Botón 8
+		btn8 = new JButton("8");
 		btn8.addActionListener(listenerBotonNumerico);
 		btn8.setBackground(new Color(250, 128, 114));
 		contentPane.add(btn8);
 
-		JButton btn9 = new JButton("9");
+		// Botón 9
+		btn9 = new JButton("9");
 		btn9.addActionListener(listenerBotonNumerico);
 		btn9.setBackground(new Color(250, 128, 114));
 		contentPane.add(btn9);
 
+		// Botón +
 		btnSuma = new JButton("+");
-		btnSuma.addActionListener(listenerBotonOperacion);		
+		btnSuma.addActionListener(listenerBotonOperacion);
 		btnSuma.setBackground(new Color(189, 183, 107));
 		contentPane.add(btnSuma);
 
-		JButton btn4 = new JButton("4");
+		// Botón 4
+		btn4 = new JButton("4");
 		btn4.addActionListener(listenerBotonNumerico);
 		btn4.setBackground(new Color(250, 128, 114));
 		contentPane.add(btn4);
 
-		JButton btn5 = new JButton("5");
+		// Botón 5
+		btn5 = new JButton("5");
 		btn5.addActionListener(listenerBotonNumerico);
 		btn5.setBackground(new Color(250, 128, 114));
 		contentPane.add(btn5);
 
-		JButton btn6 = new JButton("6");
+		// Botón 6
+		btn6 = new JButton("6");
 		btn6.addActionListener(listenerBotonNumerico);
 		btn6.setBackground(new Color(250, 128, 114));
 		contentPane.add(btn6);
 
-		JButton btnResta = new JButton("-");
-		btnResta.addActionListener(listenerBotonOperacion);	
+		// Botón -
+		btnResta = new JButton("-");
+		btnResta.addActionListener(listenerBotonOperacion);
 		btnResta.setBackground(new Color(189, 183, 107));
 		contentPane.add(btnResta);
 
-		JButton btn1 = new JButton("1");
+		// Botón 1
+		btn1 = new JButton("1");
 		btn1.addActionListener(listenerBotonNumerico);
 		btn1.setBackground(new Color(250, 128, 114));
 		contentPane.add(btn1);
 
-		JButton btn2 = new JButton("2");
+		// Botón 2
+		btn2 = new JButton("2");
 		btn2.addActionListener(listenerBotonNumerico);
 		btn2.setBackground(new Color(250, 128, 114));
 		contentPane.add(btn2);
 
-		JButton btn3 = new JButton("3");
+		// Botón 3
+		btn3 = new JButton("3");
 		btn3.addActionListener(listenerBotonNumerico);
 		btn3.setBackground(new Color(250, 128, 114));
 		contentPane.add(btn3);
 
-		JButton btnMultiplicacion = new JButton("X");
-		btnMultiplicacion.addActionListener(listenerBotonOperacion);	
+		// Botón X
+		btnMultiplicacion = new JButton("X");
+		btnMultiplicacion.addActionListener(listenerBotonOperacion);
 		btnMultiplicacion.setBackground(new Color(189, 183, 107));
 		contentPane.add(btnMultiplicacion);
 
-		JButton btnClear = new JButton("C");
+		// Botón C
+		btnClear = new JButton("C");
+		btnClear.addActionListener(listenerBotonClear);
 		btnClear.setBackground(new Color(240, 230, 140));
 		contentPane.add(btnClear);
 
+		// Botón 0
 		JButton btn0 = new JButton("0");
 		btn0.addActionListener(listenerBotonNumerico);
 		btn0.setBackground(new Color(250, 128, 114));
 		contentPane.add(btn0);
 
-		JButton btnComa = new JButton(".");
-		btnComa.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (!textField.getText().contains(".")) {
-					textField.setText(textField.getText() + ((AbstractButton) e.getSource()).getText());
-				}
-			}
-		});
+		// Botón .
+		btnComa = new JButton(".");
+		btnComa.addActionListener(listenerBotonComa);
 		btnComa.setBackground(new Color(240, 230, 140));
 		contentPane.add(btnComa);
 
-		JButton btnDivision = new JButton("/");
-		btnDivision.addActionListener(listenerBotonOperacion);	
+		// Botón /
+		btnDivision = new JButton("/");
+		btnDivision.addActionListener(listenerBotonOperacion);
 		btnDivision.setBackground(new Color(189, 183, 107));
 		contentPane.add(btnDivision);
 
 	}
 
-	public class ListenerBotonNumerico implements ActionListener {
+	/**
+	 * Clase para crear los listener de los botones numéricos.
+	 */
+	private class ListenerBotonNumerico implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-
-				textField.setText(textField.getText() + ((AbstractButton) e.getSource()).getText());
-			
+			String valor = ((AbstractButton) e.getSource()).getText();
+			textField.setText(textField.getText() + valor);
 		}
 	}
-	
-	public class ListenerBotonOperacion implements ActionListener{
+
+	/**
+	 * Clase para crear los listener de los botones de operaciones.
+	 */
+	private class ListenerBotonOperacion implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			num1 = textField.getText();	
+			num1 = textField.getText();
 			textField.setText("");
 			operacion = ((AbstractButton) e.getSource()).getText();
-			((AbstractButton) e.getSource()).setBackground(new Color(0,255,255));
-			lblOperacion.setText(operacion);			
+			lblOperacion.setText(operacion);
+			btnSuma.setEnabled(false);
+			btnResta.setEnabled(false);
+			btnMultiplicacion.setEnabled(false);
+			btnDivision.setEnabled(false);
 		}
-		
+	}
+
+	/**
+	 * Clase para crear el listener del botón =.
+	 */
+	private class ListenerBotonIgual implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			num2 = textField.getText();
+			double resultado = 0;
+			lblOperacion.setText("=");
+			try {
+				switch (operacion) {
+				case "+":
+					resultado = (Double.parseDouble(num1) + Double.parseDouble(num2));
+					break;
+				case "-":
+					resultado = (Double.parseDouble(num1) - Double.parseDouble(num2));
+					break;
+				case "X":
+					resultado = (Double.parseDouble(num1) * Double.parseDouble(num2));
+					break;
+				case "/":
+					resultado = (Double.parseDouble(num1) / Double.parseDouble(num2));
+					break;
+
+				default:
+					break;
+				}
+			} catch (NumberFormatException e1) {
+				e1.printStackTrace();
+			}
+			textField.setText(String.valueOf(resultado));
+		}
+	}
+
+	/**
+	 * Clase para crear el listener del botón .
+	 */
+	private class ListenerBotonComa implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (!textField.getText().contains(".")) {
+				textField.setText(textField.getText() + ((AbstractButton) e.getSource()).getText());
+			}
+		}
+	}
+
+	/**
+	 * Clase para crear el listener del botón C.
+	 */
+	private class ListenerBotonClear implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			textField.setText("");
+			lblOperacion.setText("");
+			num1 = "0";
+			num2 = "0";
+			btnSuma.setEnabled(true);
+			btnResta.setEnabled(true);
+			btnMultiplicacion.setEnabled(true);
+			btnDivision.setEnabled(true);
+		}
 	}
 }
